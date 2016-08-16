e8ff46f06f403da03a2ffeade01521b95bfd73e7
Andrea Jemmett <andreajemmett@gmail.com>
2016-08-16 08:58:01 +0200

    cleaned up

--- build.sbt
+++ build.sbt
@@ -8,8 +8,6 @@ scalaVersion := "2.11.7"
 
 libraryDependencies ++= Seq(
   jdbc,
-  cache,
-  ws,
   "org.scalatestplus.play" %% "scalatestplus-play" % "1.5.1" % Test
 )
 
--- test/ApplicationSpec.scala
+++ test/ApplicationSpec.scala
@@ -1,12 +1,8 @@
 import org.scalatestplus.play._
-import play.api.test._
 import play.api.test.Helpers._
+import play.api.test._
 
-/**
- * Add your spec here.
- * You can mock out a whole application including requests, plugins etc.
- * For more information, consult the wiki.
- */
+
 class ApplicationSpec extends PlaySpec with OneAppPerTest {
 
   "Routes" should {
@@ -25,16 +21,6 @@ class ApplicationSpec extends PlaySpec with OneAppPerTest {
       status(home) mustBe OK
       contentType(home) mustBe Some("text/html")
       contentAsString(home) must include ("Your new application is ready.")
-    }
-
-  }
-
-  "CountController" should {
-
-    "return an increasing count" in {
-      contentAsString(route(app, FakeRequest(GET, "/count")).get) mustBe "0"
-      contentAsString(route(app, FakeRequest(GET, "/count")).get) mustBe "1"
-      contentAsString(route(app, FakeRequest(GET, "/count")).get) mustBe "2"
     }
 
   }
