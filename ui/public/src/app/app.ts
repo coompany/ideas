b39a87372ad37e4ab64895b3d20d0e0bea4af926
Andrea Jemmett <andreajemmett@gmail.com>
2016-08-19 20:56:11 +0200

    basic login in ui

--- ui/public/src/app/app.ts
+++ ui/public/src/app/app.ts
@@ -20,36 +20,43 @@ class User {
 
 class AppCtrl {
 
-    public user: User = null;
-    private token: string = null;
+    user: User = null;
+    loginUrl = 'http://localhost:9000/oauth2/access_token?grant_type=implicit&client_id=123&redirect_uri=http://localhost:3000/oauth/';
+    token: string = null;
 
     constructor(private $log: ng.ILogService,
                 private $state: angular.ui.IStateService,
-                private $location: ng.ILocationService) {
-        this.$log.info('AppCtrl started');
+                private $location: ng.ILocationService,
+                private $rootScope: ng.IRootScopeService) {
+        $log.info('AppCtrl started');
 
+        this.checkToken();
+        $rootScope.$on('oauthTokenUpdated', this.checkToken)
+    }
+
+    checkToken() {
         let token = localStorage.getItem('oauth.token');
         if (token) {
             this.token = token;
-            $log.debug(`Retrieved token: ${this.token}`)
+            this.$log.debug(`Retrieved token: ${this.token}`)
         }
-
     }
 
 }
 
-app.controller('AppCtrl', ['$log', '$state', '$location', AppCtrl]);
+app.controller('AppCtrl', ['$log', '$state', '$location', '$rootScope', AppCtrl]);
 
 
 class OAuthCtrl {
-    constructor($location: ng.ILocationService, $state: angular.ui.IStateService) {
+    constructor($location: ng.ILocationService, $state: angular.ui.IStateService, $scope: ng.IScope) {
         let token = $location.hash();
         localStorage.setItem('oauth.token', token);
+        $scope.$emit('oauthTokenUpdated');
         $state.go('app.index')
     }
 }
 
-app.controller('OAuthCtrl', ['$location', '$state', OAuthCtrl]);
+app.controller('OAuthCtrl', ['$location', '$state', '$scope', OAuthCtrl]);
 
 
 app.config(['$stateProvider', '$urlRouterProvider', '$locationProvider', (
