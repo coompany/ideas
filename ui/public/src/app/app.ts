16e21adfed479f6aa385fab5e254873fa766bcb2
Andrea Jemmett <andreajemmett@gmail.com>
2016-08-20 15:48:41 +0200

    ideas module & handle token expiration

--- ui/public/src/app/app.ts
+++ ui/public/src/app/app.ts
@@ -1,107 +1,191 @@
 /// <reference path="../../../typings/index.d.ts" />
 
-var app = angular.module('ideasUiApp', [
-    'app.templates',
-    'ui.router'
-]);
+
+module app {
+
+    import IRootScopeService = angular.IRootScopeService;
+    import ILogService = angular.ILogService;
+    import IStateService = angular.ui.IStateService;
+    import ILocationService = angular.ILocationService;
+    import IHttpService = angular.IHttpService;
+    import IHttpPromiseCallbackArg = angular.IHttpPromiseCallbackArg;
+    import IStateProvider = angular.ui.IStateProvider;
+    import IUrlRouterProvider = angular.ui.IUrlRouterProvider;
+    import ILocationProvider = angular.ILocationProvider;
+    import IHttpProvider = angular.IHttpProvider;
+    import IQService = angular.IQService;
+    import IWindowService = angular.IWindowService;
+    import IIdeasService = app.ideas.IIdeasService;
+    import IScope = angular.IScope;
 
 
-interface IAppRootScope extends ng.IRootScopeService {
-    loginUrl: string
-    token: string
-    user: IUser
-}
+
+    let app = angular.module('ideasUiApp', [
+        'app.templates',
+        'app.ideas',
+        'ui.router'
+    ]);
 
 
-interface IUser {
-    id: number
-    firstName: string
-    lastName: string
-    email: string
-}
+    export interface IAppRootScope extends IRootScopeService {
+        loginUrl: string
+        token: string
+        user: IUser
+    }
 
 
-class AppCtrl {
+    export interface IUser {
+        id: number
+        firstName: string
+        lastName: string
+        email: string
+    }
 
-    constructor(private $log: ng.ILogService,
-                private $state: angular.ui.IStateService,
-                private $location: ng.ILocationService,
-                private appScope: IAppRootScope,
-                private $http: ng.IHttpService) {
 
-        this.appScope.loginUrl = 'http://localhost:9000/oauth2/access_token?grant_type=implicit&client_id=123&redirect_uri=http://localhost:3000/oauth/';
+    class AppCtrl {
 
-        if ($state.current.name != 'app.oauth') {
+        constructor(private $log: ILogService,
+                    private $state: IStateService,
+                    private appScope: IAppRootScope,
+                    private appConfig: IAppConfig,
+                    $http: IHttpService) {
+
+            this.appScope.loginUrl = appConfig.loginUrl;
+
+            if ($state.current.name != 'app.oauth') {
+                $http.get(`${appConfig.apiUrl}/me`).then((user: IHttpPromiseCallbackArg<IUser>) => {
+                    this.appScope.user = user.data;
+                });
+            }
+        }
+
+    }
+
+
+
+    export interface ITokenService {
+        token: string
+        checkToken(): void
+    }
+
+    class TokenService implements ITokenService {
+        public token: string;
+
+        constructor(private $http: IHttpProvider) {
             this.checkToken();
         }
-    }
 
-    checkToken() {
-        let token = localStorage.getItem('oauth.token');
-        if (token) {
-            this.appScope.token = token;
-            this.$log.debug(`Retrieved token: ${token}`);
-            this.$http.defaults.headers.common.Authorization = `Bearer ${token}`;
-            this.$http.get('http://localhost:9000/api/me').then((user: ng.IHttpPromiseCallbackArg<IUser>) => {
-                this.appScope.user = user.data;
-            })
+        checkToken() {
+            let token = localStorage.getItem('oauth.token');
+            if (token) {
+                this.token = token;
+                this.$http.defaults.headers.common.Authorization = `Bearer ${token}`;
+            } else {
+                this.token = null;
+            }
         }
     }
 
-}
-
-app.controller('AppCtrl', ['$log', '$state', '$location', '$rootScope', '$http', AppCtrl]);
 
 
-class OAuthCtrl {
-    constructor($location: ng.ILocationService, $state: angular.ui.IStateService) {
-        let token = $location.hash().split('token=')[1];
-        localStorage.setItem('oauth.token', token);
-        $state.go('app.index', {}, { reload: true })
+    class OAuthCtrl {
+        constructor($location: ILocationService, $state: IStateService, tokenService: ITokenService) {
+            let token = $location.hash().split('token=')[1];
+            localStorage.setItem('oauth.token', token);
+            tokenService.checkToken();
+            $state.go('app.index', {}, { reload: true })
+        }
     }
-}
-
-app.controller('OAuthCtrl', ['$location', '$state', OAuthCtrl]);
 
 
-app.config(['$stateProvider', '$urlRouterProvider', '$locationProvider', '$httpProvider', (
-    $stateProvider: angular.ui.IStateProvider,
-    $urlRouterProvider: angular.ui.IUrlRouterProvider,
-    $locationProvider: ng.ILocationProvider,
-    $httpProvider: ng.IHttpProvider) => {
 
-        $locationProvider.html5Mode(true);
+    export interface IAppConfig {
+        clientId: string
+        redirectUri: string
+        loginUrl: string
+        apiUrl: string
+    }
 
-        $urlRouterProvider.otherwise('/');
-
-        $stateProvider
-            .state('app', {
-                abstract: true,
-                controller: 'AppCtrl',
-                controllerAs: 'app',
-                templateUrl: 'app-templates/app/main.html'
-            })
-            .state('app.oauth', {
-                url: '/oauth/',
-                controller: 'OAuthCtrl'
-            })
-            .state('app.index', {
-                url: '/',
-                templateUrl: 'app-templates/index/index.html'
-            });
+    class AppConfig implements IAppConfig {
+        public clientId = '123';
+        public redirectUri = 'http://localhost:3000/oauth/';
+        public loginUrl = `http://localhost:9000/oauth2/access_token?grant_type=implicit&client_id=${this.clientId}&redirect_uri=${this.redirectUri}`;
+        public apiUrl = 'http://localhost:9000/api';
+    }
 
 
-        $httpProvider.interceptors.push(['$q', '$window', '$rootScope',
-            ($q: ng.IQService, $window: ng.IWindowService, appScope: IAppRootScope) => {
-                return {
-                    'responseError': <T>(response: ng.IHttpPromiseCallbackArg<T>) => {
-                        if (response.status === 401) {
-                            $window.location.href = appScope.loginUrl;
+
+    app.config(['$stateProvider', '$urlRouterProvider', '$locationProvider', '$httpProvider', (
+        $stateProvider: IStateProvider,
+        $urlRouterProvider: IUrlRouterProvider,
+        $locationProvider: ILocationProvider,
+        $httpProvider: IHttpProvider) => {
+
+            $locationProvider.html5Mode(true);
+
+            $urlRouterProvider.otherwise('/');
+
+            $stateProvider
+                .state('app', {
+                    abstract: true,
+                    controller: 'AppCtrl',
+                    controllerAs: 'app',
+                    templateUrl: 'app-templates/app/main.html'
+                })
+                .state('app.oauth', {
+                    url: '/oauth/',
+                    controller: 'OAuthCtrl'
+                })
+                .state('app.index', {
+                    url: '/',
+                    controller: 'IdeasCtrl',
+                    controllerAs: 'ideas',
+                    templateUrl: 'app-templates/ideas/index.html',
+                    resolve: {
+                        ideas: ['IdeasService', (ideasService: IIdeasService) => {
+                            return ideasService.all();
+                        }]
+                    }
+                })
+                .state('app.new', {
+                    url: '/new',
+                    controller: 'NewIdeaCtrl',
+                    controllerAs: 'ideasNew',
+                    templateUrl: 'app-templates/ideas/new.html'
+                });
+
+
+
+            let token = localStorage.getItem('oauth.token');
+            if (token) {
+                $httpProvider.defaults.headers.common.Authorization = `Bearer ${token}`;
+            }
+
+            $httpProvider.interceptors.push(['$q', '$window', 'appConfig',
+                ($q: IQService, $window: IWindowService, appConfig: IAppConfig) => {
+                    return {
+                        'responseError': <T>(response: IHttpPromiseCallbackArg<T>) => {
+                            if (response.status === 401 ||
+                                response.headers('WWW-Authenticate').indexOf('invalid_request') != -1) {
+                                $window.location.href = appConfig.loginUrl;
+                            }
+                            return $q.reject(response);
                         }
-                        return $q.reject(response);
                     }
                 }
-        }]);
+            ]);
 
-    }
-]);
+        }
+    ]);
+
+
+
+
+    app.controller('AppCtrl', ['$log', '$state', '$rootScope', 'appConfig', '$http', AppCtrl]);
+    app.controller('OAuthCtrl', ['$location', '$state', 'TokenService', OAuthCtrl]);
+
+    app.service('TokenService', ['$http', TokenService]);
+
+    app.constant('appConfig', new AppConfig());
+
+}
---
+++ ui/public/src/ideas/ideas.ts
@@ -0,0 +1,69 @@
+module app.ideas {
+
+    import IHttpPromiseCallbackArg = angular.IHttpPromiseCallbackArg;
+    import IHttpPromise = angular.IHttpPromise;
+    import IHttpService = angular.IHttpService;
+    import IStateService = angular.ui.IStateService;
+
+
+
+    export interface IIdea {
+        id: number
+        description: string
+        creator: IUser
+        createdAt: Date
+    }
+    class Idea implements IIdea {
+        id: number = null;
+        description: string = null;
+        creator: IUser = null;
+        createdAt: Date = null;
+    }
+
+
+    class IdeasCtrl {
+        ideas: IIdea[];
+        constructor(ideas: IHttpPromiseCallbackArg<IIdea[]>) {
+            this.ideas = ideas.data;
+        }
+    }
+
+
+    class NewIdeaCtrl {
+        idea: IIdea =  new Idea();
+        constructor(private ideaService: IIdeasService,
+                    private $state: IStateService) {}
+
+        save(idea: IIdea) {
+            this.ideaService.create(idea).then(() => {
+                this.$state.go('app.index', {}, { reload: true });
+            });
+        }
+    }
+
+
+    export interface IIdeasService {
+        all(): IHttpPromise<IIdea[]>
+        create(idea: IIdea): IHttpPromise<IIdea>
+    }
+
+    class IdeasService implements IIdeasService {
+
+        constructor(private $http: IHttpService) {}
+
+        all(): IHttpPromise<IIdea[]> {
+            return this.$http.get('http://localhost:9000/api/ideas');
+        }
+
+        create(idea: IIdea): IHttpPromise<IIdea> {
+            return this.$http.post('http://localhost:9000/api/ideas', idea);
+        }
+
+    }
+
+    let ideasModule = angular.module('app.ideas', []);
+    ideasModule.controller('IdeasCtrl', ['ideas', IdeasCtrl]);
+    ideasModule.controller('NewIdeaCtrl', ['IdeasService', '$state', NewIdeaCtrl]);
+    ideasModule.service('IdeasService', ['$http', IdeasService])
+
+}
